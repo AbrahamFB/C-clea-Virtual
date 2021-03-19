a3ec6b1b87fb336f4449069e3076fe1c253a2345
Abraham Flores Basilio <44345256+AbrahamFB@users.noreply.github.com>
2021-03-19 14:49:17 -0600

    avance poco con el inicio

--- banner.php
+++
@@ -1,4 +0,0 @@
-<!-- -->
-<div class="banner-index">
-
-</div>
--- nav-bar_index.php
+++ nav-bar_index.php
@@ -1,5 +1,5 @@
  <!-- Inicio sección navbar -->
  <nav id="main-nav">
-    
+     <p class="centrar-texto">MENU</p>
  </nav>
  <!-- Fin sección navbar -->
