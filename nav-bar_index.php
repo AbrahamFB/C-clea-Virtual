d61293831b796c7f8740ebd9f1c1146814a13335
Abraham Flores Basilio <44345256+AbrahamFB@users.noreply.github.com>
2021-03-19 18:28:45 -0600

    avance con el menú

--- nav-bar_index.php
+++ nav-bar_index.php
@@ -1,14 +1,25 @@
  <!-- Inicio sección navbar -->
  <header>
      <div class="container">
-         <div class="row">
+         <div class="row navegacion-principal">
              <div class="col col-3"><a href="">
                      <img src="img\logo\logo.jpg" alt="" class="logo-menu">
                  </a></div>
              <div class="col col-9 " style="text-align: right;">
-                 <nav id="main-nav">
-                     <ul>
-                         <li><a href="/">Inicio</a></li>
+                 <nav id="main-nav" class="">
+                     <ul class="nav nav-pills">
+                         <li class="nav-item">
+                             <a class="nav-link " aria-current="page" href="/">Inicio</a>
+                         </li>
+
+                         <li class="nav-item">
+                             <a class="nav-link" href="/servicios.php">Servicios</a>
+                         </li>
+                         <li class="nav-item">
+                             <a class="nav-link" href="/sobre-nosotros.php">Sobre Nosotros</a>
+                         </li>
+
+
                      </ul>
                  </nav>
              </div>
