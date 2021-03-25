7524d72b6b576db27e99ce48daca6c2e1ed01746
Abraham Flores Basilio <44345256+AbrahamFB@users.noreply.github.com>
2021-03-25 10:33:34 -0600

    Update verInsercion.php

--- verInsercion.php
+++ verInsercion.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Example</title>
+    <title>Ver Inserciones</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Latest compiled and minified CSS -->
