91a7f6a90ffbc6f9e07e8faf0412277888771090
Angel Blue <79714714+angel-blue@users.noreply.github.com>
2021-03-25 11:03:12 -0600

    Update perfil.php

--- perfil.php
+++ perfil.php
@@ -1,13 +1,20 @@
 <?php
 
+$anadirURL = "";
+$nombrePagina = "Cóclea Virtual - Perfil";
+$css_extra = "";
+
 include('html.php');
 include('nav-bar_index.php');
+
+
+
+
 
 ?>
 <?php
 include("footer.php");
 
-include("scripts.php");
 
 echo '  </body>
 </html>';
