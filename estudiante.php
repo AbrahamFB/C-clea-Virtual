7334631d8dda4dfc090865d81e2697a8e3359d5f
liz0611 <53869386+liz0611@users.noreply.github.com>
2021-04-07 16:23:45 -0500

    Subir archivo estudiante

--- estudiante.php
+++ estudiante.php
@@ -1,14 +1,9 @@
 <?php
-include("bd.php");
-$conexion = new ConexionBD();
-
-
 
 //Seguridad de sesiones 
 session_start(); //Iniciar sesión
 error_reporting(0);
 $varSesion = $_SESSION['correo'];
-$datosUsuario = $conexion->i($varSesion);
 if ($varSesion == null || $varSesion = '') {
     header("location:login_index.php");
     die();
@@ -17,7 +12,7 @@ if ($varSesion == null || $varSesion = '') {
 $anadirURL = "";
 $nombrePagina = "Cóclea Virtual - Estudiante";
 $css_extra = "";
-$_SESSION['user'] = $datosUsuario[0];
+$user = $_SESSION['correo'];
 $descripcion = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, deserunt placeat! Vel iure dolor culpa, cumque omnis quasi repellendus ab cupiditate dignissimos, autem dicta magnam maxime minima excepturi, possimus consectetur!";
 
 include("html.php");
@@ -28,9 +23,10 @@ include("nav-bar_index.php");
 
 ?>
 <div class="container">
-
+    <div class=""> <a href="logout.php" class="btn btn-danger float-right">Cierra la sesión</a>
+    </div>
     <div class="banner-estudiante">
-        <h1 class="text-center tituloPagina mayusculas">Bienvenido <?php echo $_SESSION['user'] ?></h1>
+        <h1 class="text-center tituloPagina mayusculas">Bienvenido <?php echo $user ?> a Cóclea Virtual</h1>
 
         <div class="row row-cols-2 ss">
             <div class="col">
@@ -38,11 +34,50 @@ include("nav-bar_index.php");
             </div>
             <div class="col">
                 <h2>Transcripción de Audio</h2>
-                    
+
+                <!--PARTE DE SUBIR ARCHIVO-->
+                <!-- FORM -->
+                <br>
+                <?php
+                if (isset($_SESSION['message']) && $_SESSION['message']) {
+                    printf('<b>%s</b>', $_SESSION['message']);
+                    unset($_SESSION['message']);
+                }
+                ?>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-12 p-5 bg-white shadow-lg rounded">
+
+                            <div class="margin-tb contenido-centrado">
+                                <form action="uploadAr.php" method="post" enctype="multipart/form-data" id="formTrans">
+                                    <h2 class="centrar-texto ">Sube tu archivo</h2>
+                                    <br>
 
 
+
+                                    <div class="drag-drop">
+                                        <input type="file" name="uploadedFile" multiple="multiple" id="photo" />
+                                        <!--input type="file" name="uploadedFile" multiple="multiple" id="photo" accept="application/pdf" /-->
+                                        <span class="fa-stack fa-2x">
+                                            <i class="fa fa-cloud fa-stack-2x bottom pulsating"></i>
+                                            <i class="fa fa-circle fa-stack-1x top medium"></i>
+                                            <i class="fa fa-arrow-circle-up fa-stack-1x top"></i>
+                                        </span>
+                                        <span class="desc">Pulse aquí para añadir archivos</span>
+                                    </div>
+
+                                    <input type="submit" form="formTrans" id="smtArchi" name="uploadBtn" value="Enviar" class="font-2rem btn btn btn-success btn">
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <br>
             </div>
         </div>
+        <!--PARTE DE SUBIR ARCHIVO-->
         <h3 class="text-center">Descripción</h3>
         <?php echo $descripcion; ?>
 
---
+++ uploadAr.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+
+$message = '';
+include("bd/bd.php");
+//$cuenta; // ID DEL TRANSCRIPTOR
+//$tema = $_POST('tem');
+//$nLSM; /// POR CHECAR
+//$anioEXP = $_POST('anioExp');
+//$varSesion = $_SESSION['correo'];
+//if ($varSesion == null || $varSesion = '') {
+//    header("location:login_index.php");
+//    die();
+//}
+
+
+if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Enviar') {
+    if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK) {
+        // get details of the uploaded file
+        $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
+        $fileName = $_FILES['uploadedFile']['name'];
+        $fileSize = $_FILES['uploadedFile']['size'];
+        $fileType = $_FILES['uploadedFile']['type'];
+        $fileNameCmps = explode(".", $fileName);
+        $fileExtension = strtolower(end($fileNameCmps));
+
+        // sanitize file-name
+        $newFileName = "Multimedia" . "_" . $fileName . '.' . $fileExtension;
+
+        // check if file has one of the following extensions
+        $allowedfileExtensions = array('mp4');
+
+        if (in_array($fileExtension, $allowedfileExtensions)) {
+            // directory in which the uploaded file will be moved
+            $uploadFileDir = './archivosmultimedia/';
+            $dest_path = $uploadFileDir . $newFileName;
+
+            if (move_uploaded_file($fileTmpPath, $dest_path)) {
+                $message = 'Tu archivo se subió correctamente.';
+            } else {
+                $message = 'Hubo un problema al subir tu archivo a nuestra plataforma';
+            }
+        } else {
+            $message = 'Error en el tipo de archivo:' . implode(',', $allowedfileExtensions);
+        }
+    } else {
+        $message = 'Nuestro error es al subir tu archivo a nuestra plataforma. Mandanos éste error.<br>';
+        $message .= 'Error:' . $_FILES['uploadedFile']['error'];
+    }
+
+      
+    
+$query = "INSERT INTO ArchivoMultimedia (ruta,formato,tamanio) VALUES ('$fileName','$fileType','$fileSize')"; 
+    $conexion->query($query);
+    echo $query; 
+
+}
+//$_SESSION['message'] = $message;
+//header("Location: index.php"); 
+
+?>
