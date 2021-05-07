ea81869fd23f133c3b97f1277dec0f79c0b700ac
Abraham Flores Basilio <44345256+AbrahamFB@users.noreply.github.com>
2021-05-07 08:36:15 -0500

    Revert "avance rechazados"
    This reverts commit 3917bb09e16d2bce531069af787374f9ed375bf5.

--- p2.php
+++ p2.php
@@ -1,5 +1,5 @@
 <?php
-
+//hay que agregar que tambien cambie el idTranscriptor del que lo eligió
 session_start();
 include("bd/bd.php");
 include("bd.php");
@@ -8,17 +8,9 @@ $conexion2 = new ConexionBD();
 $id = $_POST['id'];
 
 
-    //cambiamos el estado del archivo a "pendiente" nuevamente
-    //para que le aparezca al verificador
+
     $archivos =  "UPDATE ArchivoTranscrito SET estado = '2' WHERE idArchivoTranscrito = '$id'";
     $res = mysqli_query($conexion, $archivos);
     echo $res;
     header("location:transcriptor.php");
-
-$carpeta = "usuarios/'".$_SESSION['correoVideo']."'/ArchivoTranscrito/";
-opendir($carpeta);
-$destino = $carpeta.$_FILES['foto']['name'];
-copy($_FILES['foto']['tmp_name'],$destino);
-
-header("Location: transcriptor.php");
 ?>
