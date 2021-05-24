83420d32cb56624cdaecf4f79aed260f6871de14
Abraham Flores Basilio <44345256+AbrahamFB@users.noreply.github.com>
2021-05-24 08:51:26 -0500

    delete

--- bd/bd.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conexion = mysqli_connect("162.241.62.191", "tecnoso5_master", "nKwuIMe#Nj*8", "tecnoso5_cocleavirtual");
-?>
--- dashboard/js/demo/updateArchTran.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    include('bd.php');
-    $id = $_POST['id'];
-    $estado = $_POST['validado'];
-    $sql = "UPDATE ArchivoTranscrito SET estado = '$estado' WHERE idArchivoTranscrito = '$id'";
-    $conexion->query($sql);
-    
-?>
